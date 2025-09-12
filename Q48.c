467cbc407bea849b2bb9edff0d58c6eac627aceb
K Arya <aryapandey723@gmail.com>
2025-09-13 00:19:28 +0530

    Add files via upload

---
+++ Q48.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int i, j;
+
+    for (i = 1; i <= 5; i++) {       // rows
+        for (j = 1; j <= i; j++) {   // print numbers from 1 to i
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
